a5bfe4894ade2f08536b3e30c4c033ccca63be83
MC <marie-claire.lambo@edu.devinci.fr>
2025-02-19 21:48:30 +0100

    📱 responsive

--- pages/about.vue
+++ pages/about.vue
@@ -76,24 +76,25 @@ onMounted(() => {
 
 
 <template>
-  <div class="min-h-screen bg-background-light dark:bg-background-dark text-primary-light dark:text-primary-dark flex flex-col p-8">
-    <div class="mt-[100px] lg:mt-[150px]">
-      <h1 class="typing-title text-[64px] md:text-[84px] sm:text-[48px] font-extrabold text-secondary-light dark:text-secondary-dark min-h-[130px] sm:min-h-[100px]"></h1>
-      
-      <div class="flex flex-col lg:flex-row items-center gap-12 lg:gap-0 mt-8">
-        <div class="flex flex-col gap-12 lg:-mt-[10px] w-full lg:w-1/2 text-[24px] md:text-[20px] sm:text-[18px] font-light leading-[36px] text-primary-light dark:text-primary-dark">
-          <p class="presentation-text">Je donne vie à des interfaces web claires et fonctionnelles. <br/>
-            Ce que j'aime, c'est transformer des idées en expériences utilisateur efficaces et agréables. <br/>
-            J'accorde une attention particulière à la performance, à l'esthétique et surtout à la simplicité d'utilisation.</p>
-          <p class="presentation-text">J'ai eu l'opportunité de travailler sur plusieurs projets, seule ou en alternance, grâce auxquels j'ai acquis des compétences en React, Vue.js et le développement de composants réutilisables.</p>
+  <div class="min-h-screen bg-background-light dark:bg-background-dark text-primary-light dark:text-primary-dark flex flex-col justify-center p-8">
+    <div>
+      <div class="flex flex-col lg:flex-row items-center">
+        <div class="flex flex-col gap-[2rem] lg:w-1/2">
+          <h1 class="typing-title text-[84px] md:text-[64px] sm:text-[48px] -mt-[6rem] font-extrabold text-secondary-light dark:text-secondary-dark min-h-[130px] sm:min-h-[100px]"></h1>
+          <div class="flex flex-col gap-12 w-full text-[24px] md:text-[20px] sm:text-[18px] font-light leading-[36px] text-primary-light dark:text-primary-dark">
+            <p class="presentation-text">Je donne vie à des interfaces web claires et fonctionnelles. <br/>
+              Ce que j'aime, c'est transformer des idées en expériences utilisateur efficaces et agréables. <br/>
+              J'accorde une attention particulière à la performance, à l'esthétique et surtout à la simplicité d'utilisation.</p>
+            <p class="presentation-text">J'ai eu l'opportunité de travailler sur plusieurs projets, seule ou en alternance, grâce auxquels j'ai acquis des compétences en React, Vue.js et le développement de composants réutilisables.</p>
+          </div>
         </div>
       
-        <div class="w-full lg:w-[400px] mx-auto flex flex-col">
+        <div class="w-full lg:w-1/2 mx-auto flex flex-col lg:mr-[10rem] mt-12 lg:mt-0">
           <h2 class="stack-title text-4xl md:text-3xl sm:text-2xl font-medium mb-12 text-center">Stack technique</h2>
           <div class="flex flex-col items-center gap-[5rem] md:gap-[3rem]">
             <div class="w-full flex flex-col items-center cursor-pointer" @click="showFront = !showFront">
               <Button variant="secondary" class="stack-button text-3xl font-medium py-6 px-8 dark:border dark:border-secondary-dark dark:bg-transparent"> Front-end </Button>
-              <div v-if="showFront" class="flex gap-4 mt-4 flex-wrap">
+              <div v-if="showFront" class="flex gap-4 mt-4">
                 <div v-for="tech in front_technologies" :key="tech.name">
                   <img 
                     :src="tech.image" 
