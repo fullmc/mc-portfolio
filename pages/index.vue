3835320f466dd500e7b8d98303bce7da2f361b06
MC <marie-claire.lambo@edu.devinci.fr>
2025-07-07 18:59:34 +0200

    refactor: add projects

--- pages/index.vue
+++ pages/index.vue
@@ -37,16 +37,19 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="relative overflow-hidden" id="home">
+  <div class="relative overflow-hidden flex flex-col gap-[5rem]" id="home">
     <!-- Section home -->
     <section class="relative h-screen flex flex-col items-center pt-[8rem] px-[8rem]">
+      <div class="text-orange-500 text-[1rem] w-fit bg-orange-500/10 px-4 py-2 rounded-xl">
+        {{ $t('home_dev') }}
+      </div>
       <h1 class="md:text-[8vw] tracking-wider font-semibold text-center w-full">
         {{ $t('home_id') }}
       </h1>
       <!-- Section avec disposition horizontale -->
       <div class="flex items-center justify-between w-full">
         <!-- Metrics à gauche -->
-        <div class="flex flex-col gap-6 text-left">
+        <div class="flex flex-col gap-6 text-left pl-[6rem] pt-[2rem]">
           <div class="text-orange-500 text-[2rem] flex flex-col items-center">3+<span class="block text-base text-primary-light dark:text-primary-dark">{{ $t('years_experience') }}</span></div>
           <div class="text-orange-500 text-[2rem] flex flex-col items-center">5+<span class="block text-base text-primary-light dark:text-primary-dark">{{ $t('projects_completed') }}</span></div>
           <div class="text-orange-500 text-[2rem] flex flex-col items-center">2+<span class="block text-base text-primary-light dark:text-primary-dark">{{ $t('technologies') }}</span></div>
@@ -54,16 +57,16 @@ onMounted(async () => {
 
         <!-- Photo au centre -->
         <div class="">
-          <img src="/nana-nobg.webp" alt="profile" class="h-[85vh] absolute bottom-0 transform -translate-x-1/3" />
+          <img src="/nana-nobg.webp" alt="profile" class="h-[80vh] absolute bottom-0 transform -translate-x-1/3" />
         </div>
 
         <!-- Description à droite -->
-        <div class="flex flex-col justify-center gap-4 max-w-sm">
+        <div class="flex flex-col justify-center gap-4 max-w-md pr-[3rem]">
           <div class=" bg-white/90 rounded-full border mt-4 flex items-center gap-2 px-3 py-1 w-fit">
             <span class="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
             <span class="text-xs font-medium">{{ $t('available_for_work') }}</span>
           </div>
-          <p class="text-lg text-justify leading-relaxed">
+          <p class="text-lg leading-relaxed">
             {{ $t("home_description") }}
           </p>
         </div>
@@ -71,8 +74,8 @@ onMounted(async () => {
     </section>
 
     <!-- Section Projets -->
-    <section id="projects" class="min-h-screen scroll-animate">
-      <!-- <Projects /> -->
+    <section id="projects" class="mx-[8rem]">
+      <Projects />
     </section>
 
     <!-- Section A propos -->
--- pages/projects.vue
+++ pages/projects.vue
@@ -88,66 +88,20 @@ const projects = ref<IProject[]>([
   },
 ])
 
-onMounted(async () => {
-  const Velocity = (await import('velocity-animate')).default;
-  if (typeof window === 'undefined') return;
-
-  animateOnScroll(
-    '.projects-title',
-    { translateY: [0, 100], opacity: [1, 0] },
-    { duration: 1000, easing: 'easeOutCubic' }
-  );
-
-});
 </script>
 <template>
-  <div class="relative w-full overflow-hidden rounded-t-[100%] bg-[#D6CBB9] dark:border-2 dark:border-secondary-dark dark:bg-transparent pt-[10%] pb-[10%]">
-    <div class="relative z-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <h1 class="projects-title text-6xl font-normal text-primary-dark pb-[6%] -mt-[6%] text-center">
+  <div class="h-screen flex flex-col">
+    <div class="w-full">
+      <h1 class="text-5xl text-left font-normal text-primary-light dark:text-primary-dark mb-8">
         {{ $t('my_projects') }}
       </h1>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-12 pt-12">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div v-for="(project, index) in projects" 
-            :key="index" 
-            class="project-card bg-primary-dark/40 dark:bg-background-dark p-6 rounded-2xl border border-gray-200 dark:border-white/20 flex flex-col justify-between h-[300px]">
-          <div>
-            <div class="flex items-center justify-between mb-4">
-              <AnimatedGradientText>
-                <h3
-                  :class="cn(
-                    `inline animate-gradient bg-gradient-to-r from-[#E87B35] via-[#C54269] to-[#E87B35] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
-                  )" class="text-2xl font-semibold"
-                >
-                  {{ project.title }}
-                </h3>
-              </AnimatedGradientText>
-              <a v-if="project.link" :href="project.link" target="_blank" class="text-primary-dark">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                </svg>
-              </a>
-            </div>
-            <p class="text-lg text-[#877b68] dark:text-primary-dark mb-1">{{ $t(project.descriptionKey) }}</p>
-            <p class="text-sm text-[#877b68] dark:text-primary-dark italic">{{ $t(project.tasksKey) }}</p>
-          </div>
-          <div class="mt-4">
-            <div v-if="project.frontTech.length > 0" class="flex items-center mb-2">
-              <span class="text-sm font-medium text-[#877b68] dark:text-primary-dark w-16">Front:</span>
-              <div class="flex gap-3">
-                <img v-for="tech in project.frontTech" :key="tech.name" 
-                  :src="colorMode.value === 'dark' && tech.iconDark ? tech.iconDark : tech.icon" 
-                  :alt="tech.name" class="h-5 w-5" />
-              </div>
-            </div>
-            <div v-if="project.backTech.length > 0" class="flex items-center">
-              <span class="text-sm font-medium text-[#877b68] dark:text-primary-dark w-16">Back:</span>
-              <div class="flex gap-3">
-                <img v-for="tech in project.backTech" :key="tech.name" 
-                  :src="colorMode.value === 'dark' && tech.iconDark ? tech.iconDark : tech.icon" 
-                  :alt="tech.name" class="h-5 w-5" />
-              </div>
-            </div>
-          </div>
+          :key="index" 
+          class="project-card bg-primary-dark/40 dark:bg-background-dark p-6 rounded-2xl border border-gray-200 dark:border-white/20 flex flex-col justify-between h-[300px] relative">
+          <p class="text-2xl text-left font-normal text-primary-light dark:text-primary-dark absolute bottom-6 left-4">
+            {{ project.title }}
+          </p>
         </div>
       </div>
     </div>
